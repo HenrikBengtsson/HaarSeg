be4204c1efff54e06a7f9185e0fc8eada34decbd
Henrik Bengtsson <hb@aroma-project.org>
2022-11-09 18:09:03 -0800

    BUG FIX: Move library registration to NAMESPACE

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,5 @@
-# .onLoad()/.onUnload() is used when there is a NAMESPACE file
-.onLoad <- function(libname, pkgname) {
-  library.dynam(pkgname, package=pkgname, lib.loc=.libPaths())
-}
-
 .onUnload <- function(libpath) {
-  pkgname <- "HaarSeg";
-  library.dynam.unload(chname=pkgname, libpath=libpath)
+  library.dynam.unload(chname = .packageName, libpath = libpath)
 }
 
 
