5a92066cb0b8e2b3ed9de18bccb5086da77560f8
Henrik Bengtsson <hb@aroma-project.org>
2022-11-09 19:41:59 -0800

    DESCRIPTION: Fix up authors and maintainer

--- R/999.package.R
+++ R/999.package.R
@@ -13,12 +13,6 @@
 #   @see "haarSeg"
 # }
 #
-# \section{How to cite this package}{
-#  \preformatted{
-#   @eval "x <- citation('HaarSeg'); attr(x, 'textVersion')"
-#  }
-# }
-
 # \references{
 #   [1] Ben-Yaacov E. and Eldar YC. \emph{A fast and flexible method for the segmentation of aCGH data}, Bioinformatics, 2008.
 #   \url{https://www.ee.technion.ac.il/Sites/People/YoninaEldar/Info/software/HaarSeg.htm}
